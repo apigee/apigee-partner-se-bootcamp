328a0aa8b148eba92e267882d1a32b903de3e9cc
Sandeep Murusupalli <m.s.a.sandeep@gmail.com>
2015-12-07 22:44:18 -0800

    auto deploy

--- labs/Analytics Lesson 1 - Load Generator/apiproxies/runload-1/make.sh
+++ labs/Analytics Lesson 1 - Load Generator/apiproxies/runload-1/make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-apigeetool deployproxy -i -u $ae_username -p $ae_password -o $org -e test -n 'runload-1' -d . -V
+apigeetool deploynodeapp -i -u $ae_username -p $ae_password -o $org -e test -n 'runload-1' -d . -V
